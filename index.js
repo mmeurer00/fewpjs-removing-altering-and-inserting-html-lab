65f6ff3bcd33622789bfcca207f774e9a5b0cdf1
Maxine <maxinemeurer@yahoo.com>
2021-05-06 12:56:03 -0400

    commit tree 8042416606f7da3f739a15e940aec8141eb72ed2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.class = "victory";
+newHeader.innerHTML = "Maxine is the champion";
